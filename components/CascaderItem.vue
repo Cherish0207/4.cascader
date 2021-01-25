fabe41f186c96a80734bc0b602aef915e8ce4add
Cherish0207 <cheshire_1207@foxmail.com>
2021-01-25 15:41:21 +0800

    实现cascader-item组件循环嵌套

---
+++ components/CascaderItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="content">
+    <div class="content-left">
+      <div v-for="(item, key) in options" :key="key">
+        <div @click="select(item)">{{ item.label }}</div>
+      </div>
+    </div>
+    <div class="content-right" v-if="lists && lists.length">
+      <CascaderItem :options="lists" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CascaderItem',
+  props: {
+    options: {
+      type: Array,
+      default: () => []
+    } 
+  },
+  data() {
+    return {
+      currentSelect: null, // 当前点击的第一层儿子
+    }
+  },
+  computed: {
+    lists() {
+      return this.currentSelect && this.currentSelect.children;
+    },
+  },
+  methods: {
+    select(item) {
+      this.currentSelect = item;
+    },
+  }
+}
+</script>
