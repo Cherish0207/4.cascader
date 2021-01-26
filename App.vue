07a6b06d1739b85aed5cb7730a4859ad0f30d7fe
Cherish0207 <cheshire_1207@foxmail.com>
2021-01-26 09:17:37 +0800

    数据动态获取-省市联动-组件外部处理

--- App.vue
+++ App.vue
@@ -1,72 +1,36 @@
 <template>
-  <Cascader :options="options" v-model="selected"></Cascader>
+  <Cascader :options="options" v-model="selected" @input="input"></Cascader>
 </template>
 
 <script>
 import Cascader from "./components/Cascader";
+import dataList from './dataList.json';
+const fetchData = (id)=>{
+    return new Promise((resolve)=>{
+       setTimeout(()=>{
+        let result = dataList.filter(item=>item.pid == id);
+        resolve(result); // 将获取到的数据传递出去
+       },300)
+    })
+}
 export default {
   components: {
     Cascader,
   },
+  async created() {
+    this.options = await fetchData(0)
+  },
+  methods: {
+    async input(value) {
+      const currentItem = value[value.length - 1]
+      let children = await fetchData(currentItem.id)
+      this.$set(currentItem, 'children', children)
+    }
+  },
   data() {
     return {
       selected: [],
-      options: [
-        {
-          label: "肉类",
-          children: [
-            {
-              label: "猪肉",
-              children: [
-                {
-                  label: "五花肉",
-                },
-                {
-                  label: "里脊肉",
-                },
-              ],
-            },
-            {
-              label: "鸡肉",
-              children: [
-                {
-                  label: "鸡腿",
-                },
-                {
-                  label: "鸡翅",
-                },
-              ],
-            },
-          ],
-        },
-        {
-          label: "蔬菜",
-          children: [
-            {
-              label: "叶菜类",
-              children: [
-                {
-                  label: "大白菜",
-                },
-                {
-                  label: "小白菜",
-                },
-              ],
-            },
-            {
-              label: "根茎类",
-              children: [
-                {
-                  label: "萝卜",
-                },
-                {
-                  label: "土豆",
-                },
-              ],
-            },
-          ],
-        },
-      ],
+      options: [],
     };
   },
 };
